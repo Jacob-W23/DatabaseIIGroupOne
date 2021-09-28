2f07c4caaccabe330bae0528cb317f1111e03964
stephanebadoumou <61429691+stephanebadoumou@users.noreply.github.com>
2021-09-27 19:29:28 -0600

    Update 01_Group1_Ent_Struc.sql

--- 01_Group1_Ent_Struc.sql
+++ 01_Group1_Ent_Struc.sql
@@ -3,126 +3,126 @@ GO
 USE [Entertainment]
 GO
 CREATE TABLE [dbo].[Agents](
-	[Agent_ID] [nvarchar](255) NULL,
-	[Agent_F_Name] [nvarchar](255) NULL,
-	[Agent_L_Name] [nvarchar](255) NULL,
-	[Agent_Street_ Address] [nvarchar](255) NULL,
-	[Agent_City] [nvarchar](255) NULL,
-	[Agent_State] [nvarchar](255) NULL,
-	[Agent_Zip_Code] [float] NULL,
-	[Agent_Phone_Number] [nvarchar](255) NULL,
-	[Agent_Date_Hired] [datetime] NULL,
-	[Agent_Salary] [money] NULL,
-	[Agent_Commission_Rate] [float] NULL
+	[Agent_ID] [varchar](5) NOT NULL,
+	[Agent_F_Name] [varchar](50) NOT NULL,
+	[Agent_L_Name] [varchar](50) NOT NULL,
+	[Agent_Street_ Address] [varchar](50) NOT NULL,
+	[Agent_City] [varchar](15) NOT NULL,
+	[Agent_State] [varchar](2) NOT NULL,
+	[Agent_Zip_Code] [varchar](5) NOT NULL,
+	[Agent_Phone_Number] [varchar](8) NOT NULL,
+	[Agent_Date_Hired] [date] NOT NULL,
+	[Agent_Salary] [money] NOT NULL,
+	[Agent_Commission_Rate] [decimal] NOT NULL
 ) ON [PRIMARY]
 GO
 
-SET ANSI_NULLS ON
+SET ANSI_NOT NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Cust_Mus_Preferences](
-	[Cust_ID] [nvarchar](255) NULL,
-	[Music_Style_ID] [nvarchar](255) NULL,
-	[Cust_Mus_Preferences_Rating] [float] NULL
+	[Cust_ID] [varchar](5) NOT NULL,
+	[Music_Style_ID] [varchar](5) NOT NULL,
+	[Cust_Mus_Preferences_Rating] [smallint] NOT NULL
 ) ON [PRIMARY]
 GO
 
-SET ANSI_NULLS ON
+SET ANSI_NOT NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Customers](
-	[Cust_ID] [nvarchar](255) NULL,
-	[Cust_F_Name] [nvarchar](255) NULL,
-	[Cust_L_Name] [nvarchar](255) NULL,
-	[Cust_Street_Address] [nvarchar](255) NULL,
-	[Cust_City] [nvarchar](255) NULL,
-	[Cust_State] [nvarchar](255) NULL,
-	[Cust_Zip_Code] [float] NULL,
-	[Cust_Phone_Number] [nvarchar](255) NULL
+	[Cust_ID] [varchar](5) NOT NULL,
+	[Cust_F_Name] [varchar](50) NOT NULL,
+	[Cust_L_Name] [varchar](50) NOT NULL,
+	[Cust_Street_Address] [varchar](50) NOT NULL,
+	[Cust_City] [varchar](15) NOT NULL,
+	[Cust_State] [varchar](2) NOT NULL,
+	[Cust_Zip_Code] [varchar](5) NOT NULL,
+	[Cust_Phone_Number] [varchar](8) NOT NULL
 ) ON [PRIMARY]
 GO
-
-SET ANSI_NULLS ON
+/****** Object:  Table [dbo].[Engagements$]    Script Date: 9/27/2021 2:25:19 PM ******/
+SET ANSI_NOT NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Engagements](
-	[Engag_Number] [float] NULL,
-	[Engag_Start_Date] [datetime] NULL,
-	[Engag_End_Date] [datetime] NULL,
-	[Engag_Start_Time] [datetime] NULL,
-	[Engag_Stop_Time] [datetime] NULL,
-	[Engag_Contract_Price] [money] NULL,
-	[Cust_ID] [nvarchar](255) NULL,
-	[Agent_ID] [nvarchar](255) NULL,
-	[Entertainer_ID] [nvarchar](255) NULL
+	[Engag_Number] [varchar](5) NOT NULL,
+	[Engag_Start_Date] [date] NOT NULL,
+	[Engag_End_Date] [date] NOT NULL,
+	[Engag_Start_Time] [time] NOT NULL,
+	[Engag_Stop_Time] [time] NOT NULL,
+	[Engag_Contract_Price] [money] NOT NULL,
+	[Cust_ID] [varchar](5) NOT NULL,
+	[Agent_ID] [varchar](5) NOT NULL,
+	[Entertainer_ID] [varchar](5) NOT NULL
 ) ON [PRIMARY]
 GO
-
-SET ANSI_NULLS ON
+/****** Object:  Table [dbo].[Entertainers]    Script Date: 9/27/2021 2:25:19 PM ******/
+SET ANSI_NOT NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Entertainers](
-	[Entertainer_ID] [nvarchar](255) NULL,
-	[Entertainer_Stage_Name] [nvarchar](255) NULL,
-	[Entertainer_SSN] [nvarchar](255) NULL,
-	[Entertainer_Street_Address] [nvarchar](255) NULL,
-	[Entertainer_City] [nvarchar](255) NULL,
-	[Entertainer_State] [nvarchar](255) NULL,
-	[Entertainer_Zip_Code] [float] NULL,
-	[Entertainer_Phone_Number] [nvarchar](255) NULL,
-	[Entertainer_Page] [nvarchar](255) NULL,
-	[Entertainer_Email] [nvarchar](255) NULL,
-	[Entertainer_Date_Entered] [datetime] NULL
+	[Entertainer_ID] [varchar](5) NOT NULL,
+	[Entertainer_Stage_Name] [varchar](50) NOT NULL,
+	[Entertainer_SSN] [varchar](11) NOT NULL,
+	[Entertainer_Street_Address] [varchar](50) NOT NULL,
+	[Entertainer_City] [varchar](15) NOT NULL,
+	[Entertainer_State] [varchar](2) NOT NULL,
+	[Entertainer_Zip_Code] [varchar](5) NOT NULL,
+	[Entertainer_Phone_Number] [varchar](8) NOT NULL,
+	[Entertainer_Page] [varchar](50) NOT NULL,
+	[Entertainer_Email] [varchar](50) NOT NULL,
+	[Entertainer_Date_Entered] [date] NOT NULL
 ) ON [PRIMARY]
 GO
 
-SET ANSI_NULLS ON
+SET ANSI_NOT NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Entertainers_Members](
-	[Entertainer_ID] [nvarchar](255) NULL,
-	[Mbr_ID] [nvarchar](255) NULL,
-	[Entertainer_Members_Status] [float] NULL
+	[Entertainer_ID] [varchar](5) NOT NULL,
+	[Mbr_ID] [varchar](5) NOT NULL,
+	[Entertainer_Members_Status] [smallint] NOT NULL
 ) ON [PRIMARY]
 GO
 
-SET ANSI_NULLS ON
+SET ANSI_NOT NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Entertainers_Style](
-	[Entertainer_ID] [nvarchar](255) NULL,
-	[Music_Style_ID] [nvarchar](255) NULL,
-	[Member_Style_Strength] [float] NULL
+	[Entertainer_ID] [varchar](5) NOT NULL,
+	[Music_Style_ID] [varchar](5) NOT NULL,
+	[Member_Style_Strength] [SMALLINT] NOT NULL
 ) ON [PRIMARY]
 GO
 
-SET ANSI_NULLS ON
+SET ANSI_NOT NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Members](
-	[Mbr_ID] [nvarchar](255) NULL,
-	[Mbr_F_Name] [nvarchar](255) NULL,
-	[Mbr_L_Name] [nvarchar](255) NULL,
-	[Mbr_Title] [nvarchar](255) NULL,
-	[Mbr_Phone_Number] [nvarchar](255) NULL,
-	[Mbr_Gender] [nvarchar](255) NULL
+	[Mbr_ID] [varchar](5) NOT NULL,
+	[Mbr_F_Name] [varchar](50) NOT NULL,
+	[Mbr_L_Name] [varchar](50) NOT NULL,
+	[Mbr_Title] [varchar](5) NULL,
+	[Mbr_Phone_Number] [varchar](8) NOT NULL,
+	[Mbr_Gender] [varchar](1) NOT NULL
 ) ON [PRIMARY]
 GO
-
-SET ANSI_NULLS ON
+/****** Object:  Table [dbo].[Music_Style]    Script Date: 9/27/2021 2:25:19 PM ******/
+SET ANSI_NOT NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Music_Style](
-	[Music_Style_ID] [nvarchar](255) NULL,
-	[Music_Style_Name] [nvarchar](255) NULL
+	[Music_Style_ID] [varchar](5) NOT NULL,
+	[Music_Style_Name] [varchar](50) NOT NULL
 ) ON [PRIMARY]
 GO
 USE [master]
