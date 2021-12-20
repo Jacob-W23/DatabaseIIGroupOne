559e87eb0fcc2c2fda92789546dff0f6f1327c32
Jacob-W23 <74944641+Jacob-W23@users.noreply.github.com>
2021-12-19 22:51:28 -0600

    Add files via upload

--- Group1_DB_Sruct.sql
+++ Group1_DB_Sruct.sql
@@ -10,10 +10,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Foods](
 	[FoodID] [nvarchar](5) NOT NULL,
-	[FoodDescription] [nvarchar](255) NOT NULL,
+	[FoodDescription] [text] NOT NULL,
 	[FoodPrice] [money] NOT NULL,
-	[Stored] [nvarchar](255) NOT NULL,
-	[Quantity] [Int] NOT NULL,
+	[FoodStored] [text] NOT NULL,
+	[FoodQuantity] [Int] NOT NULL,
 	CONSTRAINT PK_Food PRIMARY KEY (FoodID),
 	CONSTRAINT [chk_FoodID] CHECK  ((substring([FoodID],(1),(1))='F' AND 
 	(CONVERT([int],substring([FoodID],(2),(2)))>=(0) AND CONVERT([int],substring([FoodID],(2),(2)))<=(9999))))
@@ -24,7 +24,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Inventory](
 	[InvID] [nvarchar](5) NOT NULL,
-	[InvDescription] [nvarchar](255) NOT NULL,
+	[InvDescription] [text] NOT NULL,
 	CONSTRAINT PK_Inventory PRIMARY KEY (InvID),
 	CONSTRAINT [chk_InvID] CHECK  ((substring([InvID],(1),(1))='I' AND 
 	(CONVERT([int],substring([InvID],(2),(2)))>=(0) AND CONVERT([int],substring([InvID],(2),(2)))<=(9999))))
@@ -35,8 +35,8 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Jobs](
 	[JobID] [nvarchar](5) NOT NULL,
-	[JobDescription] [nvarchar](255) NOT NULL,
-	[BasePay/Day] [money] NOT NULL,
+	[JobDescription] [varchar] (50) NOT NULL,
+	[JobBasePayPerDay] [money] NOT NULL,
 	CONSTRAINT PK_Job PRIMARY KEY (JobID),
 	CONSTRAINT [chk_JobID] CHECK  ((substring([JobID],(1),(1))='J' AND 
 	(CONVERT([int],substring([JobID],(2),(2)))>=(0) AND CONVERT([int],substring([JobID],(2),(2)))<=(9999))))
@@ -47,10 +47,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Medicine](
 	[MedicineID] [nvarchar](5) NOT NULL,
-	[MedicineDescription] [nvarchar](255) NOT NULL,
+	[MedicineDescription] [text] NOT NULL,
 	[MedicinePrice] [money] NOT NULL,
-	[Stored] [nvarchar](255) NOT NULL,
-	[Quantity] [Int] NOT NULL,
+	[MedicineStored] [text] NOT NULL,
+	[MedicineQuantity] [Int] NOT NULL,
 	CONSTRAINT PK_Medicine PRIMARY KEY (MedicineID),
 	CONSTRAINT [chk_MedicineID] CHECK  ((substring([MedicineID],(1),(1))='M' AND 
 	(CONVERT([int],substring([MedicineID],(2),(2)))>=(0) AND CONVERT([int],substring([MedicineID],(2),(2)))<=(9999))))
@@ -61,13 +61,12 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Misc](
 	[MiscID] [nvarchar](5) NOT NULL,
-	[MiscDescription] [nvarchar](255) NOT NULL,
-	[InvPrice] [money] NOT NULL,
-	[Unit] [nvarchar](255) NOT NULL,
-	[Stored] [nvarchar](255) NOT NULL,
-	[Quantity] [Int] NOT NULL,
+	[MiscDescription] [text] NOT NULL,
+	[MiscPrice] [money] NOT NULL,
+	[MiscStored] [text] NOT NULL,
+	[MiscQuantity] [Int] NOT NULL,
 	CONSTRAINT PK_Misc PRIMARY KEY (MiscID),
-	CONSTRAINT [chk_MiscID] CHECK  ((substring([MiscID],(1),(1))='M' AND 
+	CONSTRAINT [chk_MiscID] CHECK  ((substring([MiscID],(1),(1))='U' AND 
 	(CONVERT([int],substring([MiscID],(2),(2)))>=(0) AND CONVERT([int],substring([MiscID],(2),(2)))<=(9999))))
 ) ON [PRIMARY]
 GO
@@ -76,7 +75,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Zombies](
 	[ZombieID] [nvarchar](5) NOT NULL,
-	[ZombieDescription] [nvarchar](255) NOT NULL,
+	[ZombieDescription] [text] NOT NULL,
 	[ZombiePricePerKill] [money] NOT NULL,
 	CONSTRAINT PK_Zombies PRIMARY KEY (ZombieID),
 	CONSTRAINT [chk_ZombieID] CHECK  ((substring([ZombieID],(1),(1))='Z' AND 
@@ -89,7 +88,7 @@ GO
 CREATE TABLE [dbo].[Missions](
 	[MissionID] [nvarchar](5) NOT NULL,
 	[MissionDate] [datetime] NOT NULL,
-	[MissionObjective] [nvarchar](255) NOT NULL,
+	[MissionObjective] [text] NOT NULL,
 	[ZombieID] [nvarchar](5) NOT NULL,
 	[PayPerPerson] [money] NOT NULL,
 	CONSTRAINT PK_MissionID PRIMARY KEY (MissionID),
@@ -102,8 +101,8 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[People](
 	[PeopleID] [nvarchar](5) NOT NULL,
-	[PeopleFirstName] [nvarchar](255) NOT NULL,
-	[PeopleLastName] [nvarchar](255) NOT NULL,
+	[PeopleFirstName] [nvarchar](50) NOT NULL,
+	[PeopleLastName] [nvarchar](50) NOT NULL,
 	[PeopleDateOfBirth] [datetime] NOT NULL,
 	[AmountOfMoney] [money] NOT NULL,
 	CONSTRAINT PK_People PRIMARY KEY (PeopleID),
@@ -145,7 +144,7 @@ GO
 CREATE TABLE [dbo].[SettlementInventory](
 	[SettlementID] [nvarchar](5) NOT NULL,
 	[InvID] [nvarchar](5) NOT NULL,
-	[Quanitity] [Int] NOT NULL,
+	[SetQuantity] [Int] NOT NULL,
 	CONSTRAINT PK_SettlementInventory PRIMARY KEY (SettlementID, InvID)
 ) ON [PRIMARY]
 GO
@@ -154,9 +153,9 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Settlements](
 	[SettlementID] [nvarchar](5) NOT NULL,
-	[SettlementLocation] [nvarchar](255) NOT NULL,
+	[SettlementLocation] [text] NOT NULL,
 	[SettlementSecurityLevel] [Int] NOT NULL,
-	[TypeOfHousing] [nvarchar](255) NOT NULL,
+	[TypeOfHousing] [text] NOT NULL,
 	CONSTRAINT PK_Settlement PRIMARY KEY (SettlementID),
 	CONSTRAINT [chk_SettlementID] CHECK  ((substring([SettlementID],(1),(1))='S' AND 
 	(CONVERT([int],substring([SettlementID],(2),(2)))>=(0) AND CONVERT([int],substring([SettlementID],(2),(2)))<=(9999))))
@@ -170,8 +169,10 @@ CREATE TABLE [dbo].[Transactions](
 	[PeopleID] [nvarchar](5) NOT NULL,
 	[TransDateStamp] [date] NOT NULL,
 	[TransTimeStamp] [time] NOT NULL,
-	CONSTRAINT PK_Transaction PRIMARY KEY (TransID),
-	CONSTRAINT FK_People FOREIGN KEY (PeopleID) REFERENCES People(PeopleID)
+	CONSTRAINT [chk_TransID] CHECK  ((substring([TransID],(1),(1))='T' AND
+	(CONVERT([int],substring([TransID],(2),(2)))>=(0) AND CONVERT([int],substring([TransID],(2),(2)))<=(9999)))),
+	CONSTRAINT PK_Transactions PRIMARY KEY (TransID),
+	CONSTRAINT FK_Transactions FOREIGN KEY (TransID) REFERENCES Transactions(TransID)
 ) ON [PRIMARY]
 GO
 
@@ -179,10 +180,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Weapons](
 	[WeaponID] [nvarchar](5) NOT NULL,
-	[WeaponDescription] [nvarchar](255) NOT NULL,
+	[WeaponDescription] [text] NOT NULL,
 	[WeaponPrice] [money] NOT NULL,
-	[Stored] [nvarchar](255) NOT NULL,
-	[Quantity] [Int] NOT NULL,
+	[WeaponStored] [text] NOT NULL,
+	[WeaponQuantity] [Int] NOT NULL,
 	CONSTRAINT PK_Weapons PRIMARY KEY (WeaponID),
 	CONSTRAINT [chk_WeaponID] CHECK  ((substring([WeaponID],(1),(1))='W' AND 
 	(CONVERT([int],substring([WeaponID],(2),(2)))>=(0) AND CONVERT([int],substring([WeaponID],(2),(2)))<=(9999))))
@@ -193,10 +194,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Vehicles](
 	[VehicleID] [nvarchar](5) NOT NULL,
-	[VehicleDescription] [nvarchar](255) NOT NULL,
+	[VehicleDescription] [text] NOT NULL,
 	[VehiclePrice] [money] NOT NULL,
-	[Stored] [nvarchar](255) NOT NULL,
-	[Quantity] [Int] NOT NULL,
+	[VehicleStored] [text] NOT NULL,
+	[VehicleQuantity] [Int] NOT NULL,
 	CONSTRAINT PK_Vehicles PRIMARY KEY (VehicleID),
 	CONSTRAINT [chk_VehicleID] CHECK  ((substring([VehicleID],(1),(1))='V' AND 
 	(CONVERT([int],substring([VehicleID],(2),(2)))>=(0) AND CONVERT([int],substring([VehicleID],(2),(2)))<=(9999))))
@@ -212,7 +213,7 @@ CREATE TABLE [dbo].[TransactionDetails](
 	[FoodID] [nvarchar](5) NULL,
 	[WeaponID] [nvarchar](5) NULL,
 	[MiscID] [nvarchar](5) NULL,
-	[Quantity] [Int] NULL,
+	[QuantityBought] [Int] NULL,
 ) ON [PRIMARY]
 GO
 
